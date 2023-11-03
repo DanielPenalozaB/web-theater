0ff5fa2d1dc1796b1f145254da60d9e3fec28944
Daniel Penaloza <juandanielpenalozabrito@gmail.com>
2023-11-03 09:37:13 -0500

    feat: :sparkles: Update the Input component and create the '.hintrc' file
    Add the 'autoComplete' parameter to the Input component, give a list of options to select in the 'InputInterface', also disable the 'axe/form' warning for the dynamic asign of the 'autoComplete' value

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -6,28 +6,30 @@ import { InputInterface } from "./InputInterface";
 export default function Input({
 	id,
 	label,
+	placeholder,
+	autoComplete = "off",
 	value,
 	onChange,
 	type = "text",
 }: InputInterface) {
 	return (
-		<div>
+		<div className="flex flex-col gap-2">
 			<label
 				htmlFor={id}
 				className="block text-sm text-white font-medium leading-6"
 			>
 				{label}
 			</label>
-			<div className="mt-2">
+			<div>
 				<input
 					type={type}
 					name={id}
 					id={id}
 					value={value}
 					onChange={onChange}
-					placeholder="Enter your email"
-					autoComplete="given-name"
-					className="block w-full px-2 py-1.5 bg-white dark:bg-slate-800 text-gray-900 rounded-md border-0 shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-600 placeholder:text-gray-400 dark:placeholder:text-gray-200 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 outline-none"
+					placeholder={placeholder}
+					autoComplete={autoComplete}
+					className="block w-full px-2 py-1.5 bg-white dark:bg-slate-800 text-slate-900 dark:text-slate-200 rounded-lg border-0 shadow-sm ring-1 ring-inset ring-slate-300 dark:ring-slate-600 placeholder:text-slate-400 dark:placeholder:text-slate-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 outline-none"
 				/>
 			</div>
 		</div>
